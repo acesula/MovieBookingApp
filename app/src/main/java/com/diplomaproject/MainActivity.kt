92ed656e6340a0b91761d2a5c1096fa8b3a4378f
acesula <121105546+acesula@users.noreply.github.com>
2023-06-06 13:50:13 +0200

    movie detail/navBar/colors

--- app/src/main/java/com/diplomaproject/MainActivity.kt
+++ app/src/main/java/com/diplomaproject/MainActivity.kt
@@ -8,36 +8,18 @@ import com.diplomaproject.authentication.Login
 import com.diplomaproject.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var auth: FirebaseAuth
-
+    lateinit var database: FirebaseFirestore
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
-
-        val user: FirebaseUser? = auth.currentUser
-
-        if (user == null) {
-            goToLoginPage()
-        }
-        else{
-            binding.textView.text = user.email
-        }
-
-        binding.logoutBtn.setOnClickListener(){
-            auth.signOut()
-            goToLoginPage()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    private fun goToLoginPage() {
-        val intent: Intent = Intent(this, Login::class.java)
-        startActivity(intent)
-        finish()
-    }
 }
